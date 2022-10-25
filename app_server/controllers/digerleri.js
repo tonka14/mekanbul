ec6d9a99bd6d099ce6d61307454462b631959658
tonka14 <adt-ton@hotmail.com>
2022-10-25 20:20:05 +0300

    tree

--- app_server/controllers/digerleri.js
+++ app_server/controllers/digerleri.js
@@ -1,8 +1,10 @@
-const hakkinda=function(req, res, next) {
-    res.render('hakkinda', { title: 'Hakkında' });
-  }
-  
 
-  module.exports={
+
+const hakkinda = function (req, res, next) {
+    res.render('hakkinda', { title: 'Hakkında'});
+}
+
+module.exports = {
+
     hakkinda
-  }
+}
